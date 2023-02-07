919db01efd5e8a392d52114257908df3559bf6a7
Umair Khalid <210051122@aston.ac.uk>
2023-02-07 16:33:50 +0000

    adding folders

---
+++ inventory-management/src/main/resources/classes/Product.java
@@ -0,0 +1,55 @@
+package com.example.inventorymanagement;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String productName, productDescription;
+    private double price;
+
+    public Product(long id, String productName, String productDescription, double price){
+        this.id = id;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getProductName(){
+        return productName;
+    }       
+
+    public String getProductDescription(){
+        return productDescription;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public void setProductName(String productName){
+        this.productName = productName;
+    }
+
+    public void setProductDescription(String productDescription){
+        this.productDescription = productDescription;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+}
